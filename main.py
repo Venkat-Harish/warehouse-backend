4a5a5c2e4d2e5e0e7b5ba80c6d0e4e059b9fc762
Venkat Harish <75791505+Venkat-Harish@users.noreply.github.com>
2026-06-07 17:37:46 +0530

    vercel app url added to allow_origins

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app = FastAPI(
 # ── CORS ─────────────────────────────────────────────────────────────────
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://warehouse-olive-omega.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
